a74b81cc9bb214cf4586f85de330083a89291777
Lautaro Ebner <lautiebner@gmail.com>
2022-04-11 19:54:55 -0300

    🧪

---
+++ laboratorio/lab02/ej1/sort.c
@@ -0,0 +1,27 @@
+#include <assert.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+#include "array_helpers.h"
+#include "sort_helpers.h"
+#include "sort.h"
+
+
+static void insert(int a[], unsigned int i) {
+    /* needs implementation */
+    unsigned int j = i;
+    while (j>0 && goes_before(a[j], a[j-1]))
+    {
+        swap(a, j, j-1);
+        j--;
+    }
+    
+
+}
+
+void insertion_sort(int a[], unsigned int length) {
+    for (unsigned int i = 1u; i < length; ++i) {
+        /* needs implementation */
+        insert(a, i);
+    }
+}
---
+++ laboratorio/lab02/ej1/sort_helpers.h
@@ -0,0 +1,17 @@
+#ifndef _SORT_HELPERS_H
+#define _SORT_HELPERS_H
+
+#include <stdbool.h>
+
+/* Abstract total order for sorting algorithms */
+bool goes_before(int x, int y); 
+
+/* Checks if the array 'a' is in ascending order */
+bool array_is_sorted(int a[], unsigned int length);
+
+/* Exchanges elements of array 'a' in the given positions 'i' and 'j'
+   Array remains the same if the two positions are the same
+*/
+void swap(int a[], unsigned int i, unsigned int j);
+
+#endif
