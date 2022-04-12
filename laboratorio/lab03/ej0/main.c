3e33db77443083b0b44f2b5025e2b70c1e5d9475
Lautaro Ebner <lautiebner@gmail.com>
2022-04-12 18:30:34 -0300

    lab 03

--- laboratorio/lab03/ej0/main.c
+++ laboratorio/lab03/ej0/main.c
@@ -3,16 +3,31 @@
 
 #define MAX_SIZE 1000
 
-static void dump(char a[], unsigned int length) {
-    printf("\"");
-    for (unsigned int j=0u; j < length; j++) {
-        printf("%c", a[j]);
+// static void dump(char a[], unsigned int length) {
+//     printf("\"");
+//     for (unsigned int j=0u; j < length; j++) {
+//         printf("%c", a[j]);
+//     }
+//     printf("\"");
+//     printf("\n\n");
+// }
+
+char *parse_filepath(int argc, char *argv[]) {
+    char *result;
+    // thegeekstuff.com/2013/01/c-argc-argv/
+
+    if (argc < 2) {
+        print_help(argv[0]);
+        exit(EXIT_FAILURE);
     }
-    printf("\"");
-    printf("\n\n");
+
+    result = argv[1];
+    return result;
 }
 
-int main(/* -- completar-- */) {
+
+
+int main(int argc, char *argv[]) {
     FILE *file;
     unsigned int indexes[MAX_SIZE];
     char letters[MAX_SIZE];
@@ -22,9 +37,12 @@ int main(/* -- completar-- */) {
     //  :
     // Debe guardarse aqui la cantidad de elementos leidos del archivo
     
-    /* -- completar -- */
+    char *filepath = NULL;
+    filepath = parse_filepath(argc, argv);
 
-    dump(sorted, length);
+    printf(filepath);
+
+    // dump(sorted, length);
 
     return EXIT_SUCCESS;
 }
