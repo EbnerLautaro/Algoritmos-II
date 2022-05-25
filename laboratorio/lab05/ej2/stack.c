7594b0d444ba7cca0e2ad93483b981cdd2b4a8cb
ebnerlautaro <lautiebner@gmail.com>
2022-05-24 23:55:57 -0300

    :pencil2:

--- laboratorio/lab05/ej2/stack.c
+++ laboratorio/lab05/ej2/stack.c
@@ -9,16 +9,26 @@ struct _s_stack {
     unsigned int capacity;  // Capacidad actual del arreglo elems
 };
 
+bool invrep(stack s) {
+    bool r = true;
+    if (s!=NULL) {
+        r = (s->size<=s->capacity);
+    } 
+    return r;
+
+}
+
 stack stack_empty() {
     stack s = (stack) malloc(sizeof(struct _s_stack));
     s->capacity = 0;
     s->size = 0;
     s->elems = NULL;
+    assert(invrep(s));
     return s;
 }
 
 stack stack_push(stack s, stack_elem e) {
-    //  || s->size == 0
+    assert(invrep(s));
     if (s->size == s->capacity) {
         unsigned int newcapacity = s->capacity==0 ? 1 : 2 * s->capacity; 
         // a s->elems le tengo que asignar el doble de memoria (el doble de capacidad) 
@@ -27,13 +37,14 @@ stack stack_push(stack s, stack_elem e) {
     } 
     s->elems[s->size] = e;
     (s->size)++;    
+    assert(invrep(s));
     return s;
 }
 
 
 
 stack stack_pop(stack s) {
-
+    assert(invrep(s));
     assert(!stack_is_empty(s));
     for (unsigned int i = 1; i < s->size; i++)
     {
@@ -41,31 +52,37 @@ stack stack_pop(stack s) {
     }
     
     (s->size)--;
+    assert(invrep(s));
     return s;
 }   
 
 
 unsigned int stack_size(stack s) {
+    assert(invrep(s));
     assert(!stack_is_empty(s));
     return s->size;
 }
 
 
 stack_elem stack_top(stack s) {
+    assert(invrep(s));
     assert(!stack_is_empty(s));
     return s->elems[0];
 }
 
 bool stack_is_empty(stack s) {
+    assert(invrep(s));
     return (s->size == 0);
 }
 
 stack_elem * stack_to_array(stack s) {
+    assert(invrep(s));
     assert(!stack_is_empty(s));
     return s->elems;
 }
 
 stack stack_destroy(stack s) {
+    assert(invrep(s));
     free(s->elems);
     free(s);
     s = NULL;
